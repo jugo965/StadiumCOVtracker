45643ee65ef09a786df0cf28508d305fb52310ce
jugo965 <113313530+jugo965@users.noreply.github.com>
2022-09-11 23:36:33 +0200

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,59 @@
+<template>
+  <div id="app"> 
+    <router-link class="navbar-brand" :to="{name: 'home'}">          
+      <a id="title"> <b>StadiumCOVtracker</b></a>              
+        </router-link>
+          <div class="navbtns">
+                  <router-link v-if="authenticated"  class="btn btn-outline nvb" to="/Unos">Unos gledatelja</router-link>
+                  <router-link v-if="authenticated"  class="btn btn-outline nvb" to="/Pregled">Pregled gledatelja</router-link>
+                  <router-link v-if="!authenticated"  class="btn btn-outline nvb" to="/Login">Login</router-link>
+                <span v-if="authenticated">
+                  <a @click="logout" id="logout" class="btn btn-outline logout" href="#">Logout</a>
+                </span>
+                <router-link v-if="!authenticated" class="btn btn-outline nvb" to="/Register">Register</router-link>
+              </div>
+        <router-view/>
+
+
+  </div>
+</template>
+
+<script type="text/javascript">
+
+export default {
+
+  methods: {
+    logout() {
+    }
+  },
+
+}
+</script>
+
+<style lang="scss">
+
+
+
+#title {
+margin-left: 10%;
+margin-bottom: 10%;
+color: rgb(0, 0, 0);
+font-size: 25px;
+font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+}
+
+
+.navbtns{
+text-align: center;
+}
+.nvb{
+font-size: 20px;
+font-weight: bold;
+}
+
+.logout{
+color: red;
+margin-left: 10%;
+}
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="appstyle">
+    <div class="home">
+  <div class="row">
+  <a class="hometxt">Welcome to <b>StadiumCOVtracker</b>. Please</a><router-link to="/Login">&nbsp;Login</router-link><a>&nbsp;or</a><router-link to="/Register">&nbsp;Register</router-link><a>&nbsp;to use this app.</a>
+</div>
+</div>
+</div>
+</template>
+
+<script>
+
+
+export default {
+  name: "home",
+};
+</script>
+
+<style>
+#appstyle{
+padding-top:50px;
+padding-left:50px;
+}
+.home{
+  margin-left: 18%;
+}
+.hometxt{
+  margin-left:6%;
+}
+</style>
